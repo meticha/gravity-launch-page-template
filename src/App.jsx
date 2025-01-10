291b984e43fdc3134cedbf4a7759c2caf7455e3f
Meticha Admin <metichahq@gmail.com>
2025-01-10 11:46:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ const App = () => {
     >
       {/* Physics Layer */}
       <div
-        className="fixed inset-0 bg-[#6A5ACD]"
+        className="fixed inset-0 bg-[#4169E1]"
         style={{ zIndex: isScrolled ? 0 : 1 }}
       >
         <BackgroundText />

31d7f322981a92c8b1a018895368006ccd98039a
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2025-01-17 19:22:54 +0530

    Google Analytics and Microsoft Clarity was added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import TagManager from "react-gtm-module";
+import Clarity from "@microsoft/clarity";
+
+const tagManagerArgs = {
+  gtmId: import.meta.env.G_TAG_KEY, // Replace with your GTM Container ID
+};
+
+TagManager.initialize(tagManagerArgs);
+const projectId = import.meta.env.VITE_CLARITY_PROJECT_ID;
+
+Clarity.init(projectId);
 
 createRoot(document.getElementById('root')).render(
   <>

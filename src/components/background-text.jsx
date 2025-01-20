e7c2d084346a012028f114d6ca2fb457b89ab847
Meticha Admin <metichahq@gmail.com>
2025-01-20 12:30:41 +0530

    Update background-text.jsx

--- src/components/background-text.jsx
+++ src/components/background-text.jsx
@@ -14,11 +14,6 @@ const BackgroundText = () => {
         Launching on 27th January!
       </p>
     </div>
-    <div>
-     <p className="text-xl sm:text-1xl md:text-2xl lg:text-2xl leading-tight font-bold text-white/70  0 max-w-xs sm:max-w-sm md:max-w-2xl lg:max-w-4xl px-4 py-8">
-      <a href="https://www.producthunt.com/products/jetpack-compose-snippets">Notify Me</a>
-     </p>
-    </div>
   );
 };
 

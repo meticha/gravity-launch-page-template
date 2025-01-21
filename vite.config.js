718dafdc941689f1d40ef9d74c71631ac42fe9fc
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2025-01-21 09:48:03 +0530

    google analytics is added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePluginRadar } from "vite-plugin-radar";
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: "G-170QFT5NHJ",
+      },
+    }),
+  ],
+  define: {
+    'import.meta.env.VITE_CLARITY_PROJECT_ID': JSON.stringify(process.env.VITE_CLARITY_PROJECT_ID),
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
